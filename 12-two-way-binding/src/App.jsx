243550b4df11abdde5c346062279a50017044640
Shantanu1011 <shantanulondhe1819@gmail.com>
2026-09-01 18:02:57 +0530

    This is basic project which defines how two way data binding works

---
+++ 12-two-way-binding/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+const App = () => {
+ 
+  const [title, setTitle] = React.useState('');
+
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    console.log("Form Submitted", title);
+    setTitle('');
+  }
+  return (
+    <div>
+      <form onSubmit={(e)=>{submitHandler(e)}}>
+        <input type="text" placeholder="Enter your name" value={title} onChange={(e)=>setTitle(e.target.value)}></input>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
