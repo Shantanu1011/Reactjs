6d133c4ea499d6955d81d7c71046538a34add93c
Shantanu1011 <shantanulondhe1819@gmail.com>
2026-09-02 17:56:12 +0530

    this project simply optimize how the local storage works ..its basically present under application section under webpage

---
+++ 15-api-calling/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+const App = () => {
+   /* const getData=async()=>{
+    const response=await axios.get('https://jsonplaceholder.typicode.com/users')
+    console.log(response.data)*/
+
+
+    const [data, setData] = useState([])
+    const getData=async()=>{
+      const response=await axios.get('https://picsum.photos/v2/list')
+      setData(response.data)
+    }
+    
+
+ 
+  return (
+    <div>
+      <button onClick={getData}>Get Data</button>
+      <div>
+        {data.map(function(elem,idx){
+          return <h3> {idx} hello {elem.author}</h3>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
